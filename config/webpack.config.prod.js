9daba6226161fd239026e59c8aa0b4eab0d122c3
Marten Truu <marten.truu@gmail.com>
2018-03-25 16:44:56 +0300

    Add necessary urls to serviceworker navigation fallback list

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -177,12 +177,7 @@ module.exports = {
                     ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
                     plugins: () => [
                       autoprefixer({
-                        browsers: [
-                          '>1%',
-                          'last 4 versions',
-                          'Firefox ESR',
-                          'not ie < 9'
-                        ]
+                        browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9']
                       })
                     ]
                   }
@@ -281,7 +276,7 @@ module.exports = {
       navigateFallback: publicUrl + '/index.html',
       // Ignores URLs starting from /__ (useful for Firebase):
       // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
-      navigateFallbackWhitelist: [/^(?!\/__).*/],
+      navigateFallbackWhitelist: [/^(?!\/__).*/, /^\/(admin|api|static\/admin|images)\//],
       // Don't precache sourcemaps (they're large) and build asset manifest:
       staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/]
     })
